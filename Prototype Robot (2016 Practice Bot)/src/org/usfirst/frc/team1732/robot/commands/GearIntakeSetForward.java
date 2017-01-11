c3f7633145146679b3d762d6e41b1cd4ca4add68
JayShower <lacrossejason@gmail.com>
2017-01-11 00:03:15 -0600

    Added proto bot code

---
+++ Prototype Robot (2016 Practice Bot)/src/org/usfirst/frc/team1732/robot/commands/GearIntakeSetForward.java
@@ -0,0 +1,25 @@
+package org.usfirst.frc.team1732.robot.commands;
+
+import org.usfirst.frc.team1732.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.InstantCommand;
+
+/**
+ *
+ */
+public class GearIntakeSetForward extends InstantCommand {
+
+	public GearIntakeSetForward() {
+		super();
+		// Use requires() here to declare subsystem dependencies
+		// eg. requires(chassis);
+		requires(Robot.gearIntake);
+	}
+
+	// Called once when the command executes
+	@Override
+	protected void initialize() {
+		Robot.gearIntake.setForward();
+	}
+
+}
