b0f43f9bb5248f153628c0d8ed161ff1804d0df5
JayShower <lacrossejason@gmail.com>
2017-01-17 18:56:39 -0600

    Added further debugging info

--- PixyVision/src/org/usfirst/frc/team1732/robot/vision/VisionThread.java
+++ PixyVision/src/org/usfirst/frc/team1732/robot/vision/VisionThread.java
@@ -2,11 +2,12 @@ package org.usfirst.frc.team1732.robot.vision;
 
 public class VisionThread implements Runnable {
 
-	public GearTarget gearTarget;
-	public BoilerTarget boilerTarget;
-	public Arduino arduino = new Arduino();
-	private String total = "";
-	private boolean found = false;
+	public GearTarget	gearTarget;
+	public BoilerTarget	boilerTarget;
+	public Arduino		arduino	= new Arduino();
+	private String		total	= "";
+	private boolean		found	= false;
+
 	@Override
 	public void run() {
 		Rectangle[] rectangles = new Rectangle[0];
@@ -25,32 +26,35 @@ public class VisionThread implements Runnable {
 						rectangles = new Rectangle[rects.length];
 						for (int j = 0; j < rects.length; j++) {
 							String[] data = rects[j].split(" ");
-							rectangles[j] = new Rectangle(Integer.parseInt(data[1]), Integer.parseInt(data[3]),
-									Integer.parseInt(data[5]), Integer.parseInt(data[7]), Integer.parseInt(data[9]));
+							rectangles[j] = new Rectangle(	Integer.parseInt(data[1]), Integer.parseInt(data[3]),
+															Integer.parseInt(data[5]), Integer.parseInt(data[7]),
+															Integer.parseInt(data[9]));
 							System.out.println(j + ": " + rectangles[j]);
 						}
 						gearTarget = GearTarget.getBestVisionTarget(rectangles);
-						System.out.println(gearTarget.boundingBox);
-						System.out.println();
+						if (gearTarget != null) {
+							System.out.println(gearTarget.boundingBox);
+							System.out.println("Distance: " + getInchesToGearPeg());
+							System.out.println();
+						}
 					}
 					total = "";
 				}
 				// change this later
-				//rectangles = new Rectangle[0];
+				// rectangles = new Rectangle[0];
 				// System.out.println(s);
 			}
-			//gearTarget = GearTarget.getBestVisionTarget(rectangles);
+			// gearTarget = GearTarget.getBestVisionTarget(rectangles);
 
-			/*if (serialLine != "") {
-				String[] rects = serialLine.split("\n");
-				Rectangle[] total = new Rectangle[rects.length];
-				for (int i = 0; i < rects.length; i++) {
-					String[] data = rects[i].split(" ");
-					total[i] = new Rectangle(Integer.parseInt(data[1]), Integer.parseInt(data[3]),
-							Integer.parseInt(data[5]), Integer.parseInt(data[7]), Integer.parseInt(data[9]));
-				}
-				gearTarget = GearTarget.getBestVisionTarget(total);
-			}*/
+			/*
+			 * if (serialLine != "") { String[] rects = serialLine.split("\n");
+			 * Rectangle[] total = new Rectangle[rects.length]; for (int i = 0;
+			 * i < rects.length; i++) { String[] data = rects[i].split(" ");
+			 * total[i] = new Rectangle(Integer.parseInt(data[1]),
+			 * Integer.parseInt(data[3]), Integer.parseInt(data[5]),
+			 * Integer.parseInt(data[7]), Integer.parseInt(data[9])); }
+			 * gearTarget = GearTarget.getBestVisionTarget(total); }
+			 */
 			// int length = 0;
 			// Rectangle[] rectangles = new Rectangle[length];
 			// calculate best gear and boiler target
@@ -62,17 +66,17 @@ public class VisionThread implements Runnable {
 		if (gearTarget == null)
 			return -1;
 		return gearTarget.getHorizontalDistance(GearTarget.GEAR_TARGET_WIDTH_INCHES, HORIZONTAL_FIELD_OF_VIEW,
-				IMAGE_WIDTH);
+												IMAGE_WIDTH);
 	}
 
 	public double getAngleToGearPeg() {
 		return gearTarget.getHorizontalAngle(HORIZONTAL_FIELD_OF_VIEW, IMAGE_WIDTH);
 	}
 
-	public static final double HORIZONTAL_FIELD_OF_VIEW = 75;
-	public static final double VERTICAL_FIELD_OF_VIEW = 47;
+	public static final double	HORIZONTAL_FIELD_OF_VIEW	= 75;
+	public static final double	VERTICAL_FIELD_OF_VIEW		= 47;
 
 	// double check
-	public static final int IMAGE_WIDTH = 320;
-	public static final int IMAGE_HEIGHT = 200;
+	public static final int	IMAGE_WIDTH		= 320;
+	public static final int	IMAGE_HEIGHT	= 200;
 }
