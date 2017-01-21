8d0d2b23f9bdcecfa03f26948c5327b0ad604f44
Tkachel101 <Tkachel101@aol.com>
2017-01-21 14:47:09 -0600

    Commit of working vision code

--- PixyVision/src/org/usfirst/frc/team1732/robot/vision/Arduino.java
+++ PixyVision/src/org/usfirst/frc/team1732/robot/vision/Arduino.java
@@ -7,8 +7,15 @@ public class Arduino {
 
 	public Arduino() {
 		try {
-			serial = new SerialPort(19200, SerialPort.Port.kUSB1);
+			serial = new SerialPort(9600, SerialPort.Port.kUSB1);
 			this.serial.disableTermination();
+			System.out.println("Arduino Starting, waiting 0.5 seconds to get data");
+           // String e = this.getData();
+            edu.wpi.first.wpilibj.Timer.delay(0.125);
+
+           // if(e.equals("h")) {
+           //     System.out.println("Arduino communications locked in");
+           // }
 		} catch (Exception e) {
 			System.out.println("something went wrong, " + e.getMessage());
 		}
@@ -17,11 +24,15 @@ public class Arduino {
 	public String getData() {
 		try {
 			String s = this.serial.readString();
-			while (!s.contains("\n"))
+			while (!s.contains("\n")){
 				s += this.serial.readString();
+				//System.out.println(s);
+			}
+			//System.out.println(s);
 			return s;
 		} catch (Exception e) {
 			System.out.println("something went wrong, " + e.getMessage());
+			//e.printStackTrace();
 			return null;
 		}
 	}
--- PixyVision/src/org/usfirst/frc/team1732/robot/vision/VisionThread.java
+++ PixyVision/src/org/usfirst/frc/team1732/robot/vision/VisionThread.java
@@ -2,18 +2,28 @@ package org.usfirst.frc.team1732.robot.vision;
 
 public class VisionThread implements Runnable {
 
-	public GearTarget	gearTarget;
-	public BoilerTarget	boilerTarget;
-	public Arduino		arduino	= new Arduino();
-	private String		total	= "";
-	private boolean		found	= false;
+	public static final double HORIZONTAL_FIELD_OF_VIEW = 75;
+	public static final double VERTICAL_FIELD_OF_VIEW = 47;
+
+	// double check
+	public static final int IMAGE_WIDTH = 320;
+	public static final int IMAGE_HEIGHT = 200;
+
+	public GearTarget gearTarget;
+	public BoilerTarget boilerTarget;
+	public Arduino arduino = new Arduino();
+	private String total = "";
+	private boolean found = false;
+	private boolean started = false;
 
 	@Override
 	public void run() {
 		Rectangle[] rectangles = new Rectangle[0];
 		while (!Thread.interrupted()) {
 			String s = arduino.getData();
-			for (int i = 0; i < s.length(); i++) {
+			if (s.contains("Starting"))
+				started = true;
+			for (int i = 0; i < s.length() && started; i++) {
 				char c = s.charAt(i);
 				if (found && c != '^')
 					total += c;
@@ -26,16 +36,24 @@ public class VisionThread implements Runnable {
 						rectangles = new Rectangle[rects.length];
 						for (int j = 0; j < rects.length; j++) {
 							String[] data = rects[j].split(" ");
-							rectangles[j] = new Rectangle(	Integer.parseInt(data[1]), Integer.parseInt(data[3]),
-															Integer.parseInt(data[5]), Integer.parseInt(data[7]),
-															Integer.parseInt(data[9]));
-							System.out.println(j + ": " + rectangles[j]);
+							try {
+								rectangles[j] = new Rectangle(Integer.parseInt(data[1]), Integer.parseInt(data[3]),
+										Integer.parseInt(data[5]), Integer.parseInt(data[7]),
+										Integer.parseInt(data[9]));
+								System.out.println(j + ": " + rectangles[j]);
+							} catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
+
+							}
 						}
-						gearTarget = GearTarget.getBestVisionTarget(rectangles);
-						if (gearTarget != null) {
-							System.out.println(gearTarget.boundingBox);
-							System.out.println("Distance: " + getInchesToGearPeg());
-							System.out.println();
+						try {
+							gearTarget = GearTarget.getBestVisionTarget(rectangles);
+							if (gearTarget != null) {
+								System.out.println(gearTarget.boundingBox);
+								System.out.println("Distance: " + getInchesToGearPeg());
+								System.out.println();
+							}
+						} catch (NullPointerException e) {
+							
 						}
 					}
 					total = "";
@@ -44,39 +62,25 @@ public class VisionThread implements Runnable {
 				// rectangles = new Rectangle[0];
 				// System.out.println(s);
 			}
-			// gearTarget = GearTarget.getBestVisionTarget(rectangles);
-
-			/*
-			 * if (serialLine != "") { String[] rects = serialLine.split("\n");
-			 * Rectangle[] total = new Rectangle[rects.length]; for (int i = 0;
-			 * i < rects.length; i++) { String[] data = rects[i].split(" ");
-			 * total[i] = new Rectangle(Integer.parseInt(data[1]),
-			 * Integer.parseInt(data[3]), Integer.parseInt(data[5]),
-			 * Integer.parseInt(data[7]), Integer.parseInt(data[9])); }
-			 * gearTarget = GearTarget.getBestVisionTarget(total); }
-			 */
-			// int length = 0;
-			// Rectangle[] rectangles = new Rectangle[length];
-			// calculate best gear and boiler target
-			// gearTarget = GearTarget.getBestVisionTarget(rectangles);
 		}
+		System.out.println("Thread inturrupted");
 	}
+	// public void run(){
+	// while(!Thread.interrupted()){
+	// String s = arduino.getData();
+	// if()
+	// System.out.println(s);
+	// }
+	// }
 
-	public double getInchesToGearPeg() {
+	public synchronized double getInchesToGearPeg() {
 		if (gearTarget == null)
 			return -1;
 		return gearTarget.getHorizontalDistance(GearTarget.GEAR_TARGET_WIDTH_INCHES, HORIZONTAL_FIELD_OF_VIEW,
-												IMAGE_WIDTH);
+				IMAGE_WIDTH);
 	}
 
-	public double getAngleToGearPeg() {
+	public synchronized double getAngleToGearPeg() {
 		return gearTarget.getHorizontalAngle(HORIZONTAL_FIELD_OF_VIEW, IMAGE_WIDTH);
 	}
-
-	public static final double	HORIZONTAL_FIELD_OF_VIEW	= 75;
-	public static final double	VERTICAL_FIELD_OF_VIEW		= 47;
-
-	// double check
-	public static final int	IMAGE_WIDTH		= 320;
-	public static final int	IMAGE_HEIGHT	= 200;
 }
