b6a4ac7da918c18a52523bf87c6529452b1f0dd1
JayShower <lacrossejason@gmail.com>
2017-02-06 20:40:14 -0600

    Score 10Balls and gear is done

--- Prototype Robot/src/org/usfirst/frc/team1732/robot/autocommands/Score10BallsAndGear.java
+++ Prototype Robot/src/org/usfirst/frc/team1732/robot/autocommands/Score10BallsAndGear.java
@@ -4,6 +4,8 @@ import org.usfirst.frc.team1732.robot.commands.Wait;
 import org.usfirst.frc.team1732.robot.commands.drivetrain.Drive1DEncoders;
 import org.usfirst.frc.team1732.robot.commands.drivetrain.Drive1DTime;
 import org.usfirst.frc.team1732.robot.commands.drivetrain.TurnWithGyro;
+import org.usfirst.frc.team1732.robot.commands.flywheel.DisableFlywheel;
+import org.usfirst.frc.team1732.robot.commands.flywheel.EnableFlywheel;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
@@ -30,13 +32,17 @@ public class Score10BallsAndGear extends CommandGroup {
 		// a CommandGroup containing them would require both the chassis and the
 		// arm.
 		double speed = 0.5;
+		double time = 5;
 		if (isRedAlliance) {
-			addSequential(new Drive1DTime(5, speed, 0));
+			addSequential(new Drive1DTime(time, speed, 0));
 		} else {
-			addSequential(new Drive1DTime(5, 0, speed));
+			addSequential(new Drive1DTime(time, 0, speed));
 		}
 		addSequential(new Wait(2));
 		// shoot commands
+		addSequential(new EnableFlywheel());
+		addSequential(new Wait(5));
+		addSequential(new DisableFlywheel());
 		// drive backwards slightly to avoid knocking wall
 		addSequential(new Drive1DEncoders(-15, -15));
 		addSequential(new TurnWithGyro(180));
