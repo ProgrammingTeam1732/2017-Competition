3e38a7ca1d9e6521b8f60256337f7559c83afde7
JayShower <lacrossejason@gmail.com>
2017-02-07 18:16:09 -0600

    Revert "Removed autogenerated commends"
    This reverts commit 68a8799ea3be41511732b8ef8e756cc5a4e09d9a.

--- Prototype Robot/src/org/usfirst/frc/team1732/robot/autocommands/Score10BallsAndGear.java
+++ Prototype Robot/src/org/usfirst/frc/team1732/robot/autocommands/Score10BallsAndGear.java
@@ -15,6 +15,22 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class Score10BallsAndGear extends CommandGroup {
 
 	public Score10BallsAndGear(boolean isRedAlliance) {
+		// Add Commands here:
+		// e.g. addSequential(new Command1());
+		// addSequential(new Command2());
+		// these will run in order.
+
+		// To run multiple commands at the same time,
+		// use addParallel()
+		// e.g. addParallel(new Command1());
+		// addSequential(new Command2());
+		// Command1 and Command2 will run in parallel.
+
+		// A command group will require all of the subsystems that each member
+		// would require.
+		// e.g. if Command1 requires chassis, and Command2 requires arm,
+		// a CommandGroup containing them would require both the chassis and the
+		// arm.
 		double speed = 0.5;
 		double time = 5;
 		if (isRedAlliance) {
