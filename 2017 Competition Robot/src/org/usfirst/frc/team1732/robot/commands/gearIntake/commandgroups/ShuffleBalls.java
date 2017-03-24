7b6a2a8388e44016992caff165c0a13635665ede
JayShower <lacrossejason@gmail.com>
2017-03-24 17:47:23 -0500

    made no check gear up, changed shuffle balls

--- 2017 Competition Robot/src/org/usfirst/frc/team1732/robot/commands/gearIntake/commandgroups/ShuffleBalls.java
+++ 2017 Competition Robot/src/org/usfirst/frc/team1732/robot/commands/gearIntake/commandgroups/ShuffleBalls.java
@@ -1,7 +1,7 @@
 package org.usfirst.frc.team1732.robot.commands.gearIntake.commandgroups;
 
 import org.usfirst.frc.team1732.robot.commands.gearIntake.base.GearIntakeSetDown;
-import org.usfirst.frc.team1732.robot.commands.gearIntake.base.GearIntakeSetUp;
+import org.usfirst.frc.team1732.robot.commands.gearIntake.base.GearIntakeSetUpNoCheck;
 import org.usfirst.frc.team1732.robot.commands.helpercommands.Wait;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
@@ -14,6 +14,6 @@ public class ShuffleBalls extends CommandGroup {
 	public ShuffleBalls() {
 		addSequential(new GearIntakeSetDown());
 		addSequential(new Wait(0.3));
-		addSequential(new GearIntakeSetUp());
+		addSequential(new GearIntakeSetUpNoCheck());
 	}
 }
--- 2017 Competition Robot/src/org/usfirst/frc/team1732/robot/commands/gearIntake/commandgroups/ShuffleBallsWithWait.java
+++ 2017 Competition Robot/src/org/usfirst/frc/team1732/robot/commands/gearIntake/commandgroups/ShuffleBallsWithWait.java
@@ -1,8 +1,10 @@
 package org.usfirst.frc.team1732.robot.commands.gearIntake.commandgroups;
 
+import org.usfirst.frc.team1732.robot.commands.gearIntake.base.GearIntakeInTime;
 import org.usfirst.frc.team1732.robot.commands.gearIntake.base.GearIntakeSetDown;
+import org.usfirst.frc.team1732.robot.commands.gearIntake.base.GearIntakeSetIn;
 import org.usfirst.frc.team1732.robot.commands.gearIntake.base.GearIntakeSetStopperIn;
-import org.usfirst.frc.team1732.robot.commands.gearIntake.base.GearIntakeSetUp;
+import org.usfirst.frc.team1732.robot.commands.gearIntake.base.GearIntakeSetUpNoCheck;
 import org.usfirst.frc.team1732.robot.commands.helpercommands.Wait;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
@@ -14,22 +16,28 @@ public class ShuffleBallsWithWait extends CommandGroup {
 
 	public ShuffleBallsWithWait() {
 		// initially put stopper down
+		addSequential(new GearIntakeSetIn());
+
 		addSequential(new GearIntakeSetDown());
 		addSequential(new Wait(0.3));
 		addSequential(new GearIntakeSetStopperIn());
 		addSequential(new Wait(0.1));
-		addSequential(new GearIntakeSetUp());
+		addSequential(new GearIntakeSetUpNoCheck());
 
 		// wait 3 sec
-		addSequential(new Wait(3));
+		addSequential(new GearIntakeInTime(1));
+		addSequential(new Wait(2));
+
 		// shuffle balls
 		addSequential(new ShuffleBalls());
 		// wait 3 sec
-		addSequential(new Wait(3));
+		addSequential(new GearIntakeInTime(1));
+		addSequential(new Wait(2));
 		// shuffle balls
 		addSequential(new ShuffleBalls());
 		// wait 3 sec
-		addSequential(new Wait(3));
+		addSequential(new GearIntakeInTime(1));
+		addSequential(new Wait(2));
 		// shuffle balls
 		addSequential(new ShuffleBalls());
 	}
