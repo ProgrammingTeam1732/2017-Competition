14e20af7ad5bca41f59e5eef4624d28657828476
Tkachel101 <Tkachel101@aol.com>
2017-03-30 19:53:56 -0500

    Untested TwoGearMiddleThenSideAuto

---
+++ 2017 Practice Bot/src/org/usfirst/frc/team1732/robot/autocommands/scoregear/twogearmiddlethensideauto/TwoGearMiddleThenSideAutoBlue.java
@@ -0,0 +1,45 @@
+package org.usfirst.frc.team1732.robot.autocommands.scoregear.twogearmiddlethensideauto;
+
+import org.usfirst.frc.team1732.robot.commands.drivetrain.SetMotorSpeed;
+import org.usfirst.frc.team1732.robot.commands.drivetrain.encoder.ClearTotalDistance;
+import org.usfirst.frc.team1732.robot.commands.drivetrain.encoder.DriveEncoders;
+import org.usfirst.frc.team1732.robot.commands.drivetrain.encoder.EncoderPlaceGear;
+import org.usfirst.frc.team1732.robot.commands.drivetrain.encoder.TurnWithEncoders;
+import org.usfirst.frc.team1732.robot.commands.gearIntake.commandgroups.GrabGear;
+import org.usfirst.frc.team1732.robot.commands.gearIntake.commandgroups.InitGearIntake;
+import org.usfirst.frc.team1732.robot.commands.vision.VisionPlaceGear;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class TwoGearMiddleThenSideAutoBlue extends CommandGroup {
+
+	public TwoGearMiddleThenSideAutoBlue() {
+		addSequential(new InitGearIntake());
+
+		addSequential(new ClearTotalDistance());
+
+		addSequential(new EncoderPlaceGear(TwoGearMiddleThenSideAutoData.DRIVE_1_SETPOINT,
+				TwoGearMiddleThenSideAutoData.DRIVE_1_SETPOINT));
+
+		addSequential(new TurnWithEncoders(TwoGearMiddleThenSideAutoData.TURN_1_ANGLE_BLUE));
+
+		addParallel(new SetMotorSpeed(TwoGearMiddleThenSideAutoData.DRIVE_2_SPEED,
+				TwoGearMiddleThenSideAutoData.DRIVE_2_SPEED));
+		addSequential(new GrabGear(true, TwoGearMiddleThenSideAutoData.GRAB_GEAR_TIMEOUT));
+
+		addSequential(new SetMotorSpeed(TwoGearMiddleThenSideAutoData.DRIVE_2_STOP,
+				TwoGearMiddleThenSideAutoData.DRIVE_2_STOP));
+
+		addSequential(new TurnWithEncoders(TwoGearMiddleThenSideAutoData.TURN_2_ANGLE_BLUE));
+
+		addSequential(new DriveEncoders(TwoGearMiddleThenSideAutoData.DRIVE_3_SETPOINT));
+
+		addSequential(new TurnWithEncoders(TwoGearMiddleThenSideAutoData.TURN_3_ANGLE_BLUE));
+
+		addSequential(new VisionPlaceGear(TwoGearMiddleThenSideAutoData.VISION_DRIVEBACK,
+				TwoGearMiddleThenSideAutoData.VISION_MAX, true));
+	}
+}
---
+++ 2017 Practice Bot/src/org/usfirst/frc/team1732/robot/autocommands/scoregear/twogearmiddlethensideauto/TwoGearMiddleThenSideAutoData.java
@@ -0,0 +1,19 @@
+package org.usfirst.frc.team1732.robot.autocommands.scoregear.twogearmiddlethensideauto;
+
+public class TwoGearMiddleThenSideAutoData {
+
+	public static final double DRIVE_1_SETPOINT = 75;
+	public static final double TURN_1_ANGLE_BLUE = 90;
+	public static final double TURN_1_ANGLE_RED = -90;
+	public static final double DRIVE_2_SPEED = .5;
+	public static final double GRAB_GEAR_TIMEOUT = 5;
+	public static final double DRIVE_2_STOP = 0;
+	public static final double TURN_2_ANGLE_BLUE = -TURN_1_ANGLE_BLUE;
+	public static final double TURN_2_ANGLE_RED = -TURN_2_ANGLE_BLUE;
+	public static final double DRIVE_3_SETPOINT = 75;
+	public static final double TURN_3_ANGLE_BLUE = -30;
+	public static final double TURN_3_ANGLE_RED = -TURN_3_ANGLE_BLUE;
+	public static final double VISION_DRIVEBACK = 15;
+	public static final double VISION_MAX = 45;
+
+}
