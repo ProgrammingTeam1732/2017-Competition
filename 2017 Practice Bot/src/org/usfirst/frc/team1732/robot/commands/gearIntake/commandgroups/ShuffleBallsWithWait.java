72c35d45f1d886c2e0d71b0fd169da86bdbf37dd
JayShower <lacrossejason@gmail.com>
2017-04-06 19:52:09 -0500

    added shuffle to teleop shoot

--- 2017 Practice Bot/src/org/usfirst/frc/team1732/robot/commands/gearIntake/commandgroups/ShuffleBallsWithWait.java
+++ 2017 Practice Bot/src/org/usfirst/frc/team1732/robot/commands/gearIntake/commandgroups/ShuffleBallsWithWait.java
@@ -9,43 +9,38 @@ import org.usfirst.frc.team1732.robot.commands.helpercommands.Wait;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
-/**
- *
- */
 public class ShuffleBallsWithWait extends CommandGroup {
 
 	public ShuffleBallsWithWait() {
+		this(15);
+	}
+
+	public ShuffleBallsWithWait(int shuffleAmount) {
 		// initially put stopper down
-		System.out.println("Shuffle Balls With Wait");
 		addSequential(new GearIntakeSetIn());
-		System.out.println("GEAR INTAKE SET IN");
+
 		addSequential(new GearIntakeSetDownNoCheck());
 		addSequential(new Wait(0.3));
 		addSequential(new GearIntakeSetStopperIn());
 		addSequential(new Wait(0.1));
 		addSequential(new GearIntakeSetUpNoCheck());
-		System.out.println("GEAR INTAKE SET UP");
 
-		// wait 3 sec
-		addSequential(new GearIntakeInTime(1));
-		addSequential(new Wait(2));
+		double IN_TIME = 1;
+		double WAIT = 0;
 
-		// shuffle balls
-		addSequential(new ShuffleBalls());
-		System.out.println("Shuffle Balls 1");
-		// wait 3 sec
-		addSequential(new GearIntakeInTime(1));
-		addSequential(new Wait(2));
-		// shuffle balls
-		addSequential(new ShuffleBalls());
-		System.out.println("Shuffle Balls 2");
-		// wait 3 sec
-		addSequential(new GearIntakeInTime(1));
-		addSequential(new Wait(2));
-		// shuffle balls
-		addSequential(new ShuffleBalls());
+		class ShuffleBallUnit extends CommandGroup {
 
-		System.out.println("Shuffle Balls 3");
+			public ShuffleBallUnit() {
+				addSequential(new GearIntakeInTime(IN_TIME));
+				addSequential(new Wait(WAIT));
+				addSequential(new ShuffleBalls());
+			}
+		}
+
+		for (int i = 0; i < shuffleAmount; i++) {
+			addSequential(new ShuffleBallUnit());
+		}
+
 	}
 
 }
