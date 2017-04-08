6e1d7f615f74cdeae37f6b039429bfb473303bba
JayShower <lacrossejason@gmail.com>
2017-04-08 00:22:44 -0500

    changed shuffleballswithwait to avoid stalling gearIntake motor

--- 2017 Practice Bot/src/org/usfirst/frc/team1732/robot/commands/ballsystem/shooting/ShuffleBallsWithWait.java
+++ 2017 Practice Bot/src/org/usfirst/frc/team1732/robot/commands/ballsystem/shooting/ShuffleBallsWithWait.java
@@ -12,36 +12,33 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class ShuffleBallsWithWait extends CommandGroup {
 
 	public ShuffleBallsWithWait() {
-		this(15);
+		this(11, 1.36);
 	}
 
-	public ShuffleBallsWithWait(int shuffleAmount) {
+	public ShuffleBallsWithWait(int shuffleAmount, double secondsPerShuffle) {
 		// initially put stopper down
 		addSequential(new GearIntakeSetIn());
-
 		addSequential(new GearIntakeSetDownNoCheck());
 		addSequential(new Wait(0.3));
 		addSequential(new GearIntakeSetStopperIn());
 		addSequential(new Wait(0.1));
 		addSequential(new GearIntakeSetUpNoCheck());
+		addSequential(new GearIntakeSetStop());
 
-		double WAIT = 0; // amount of time to wait before running gear intake in
-		double IN_TIME = 1; // amount of time to run gear intake in
-		// sum of these times is time between shuffles
+		double WAIT = secondsPerShuffle - 0.3;
+		// amount of time to wait before running gear intake in
 
 		class ShuffleBallUnit extends CommandGroup {
 
 			public ShuffleBallUnit() {
 				addSequential(new Wait(WAIT));
-				addSequential(new GearIntakeSetIn());
-				addSequential(new Wait(IN_TIME));
 
 				// shuffle balls
+				addSequential(new GearIntakeSetIn());
 				addSequential(new GearIntakeSetDownNoCheck());
 				addSequential(new Wait(0.3));
 				addSequential(new GearIntakeSetUpNoCheck());
-				if (WAIT != 0)
-					addSequential(new GearIntakeSetStop());
+				addSequential(new GearIntakeSetStop());
 			}
 		}
 
