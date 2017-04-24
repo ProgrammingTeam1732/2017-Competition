a63e4e3e768069e0b378f86e8f56bb4b8dfe62ff
JayShower <lacrossejason@gmail.com>
2017-04-24 08:03:03 -0500

    Encoder place gear uses brake no shift now

--- 2017 Practice Bot/src/org/usfirst/frc/team1732/robot/commands/placegear/EncoderPlaceGear.java
+++ 2017 Practice Bot/src/org/usfirst/frc/team1732/robot/commands/placegear/EncoderPlaceGear.java
@@ -3,8 +3,8 @@ package org.usfirst.frc.team1732.robot.commands.placegear;
 import java.util.function.DoubleSupplier;
 
 import org.usfirst.frc.team1732.robot.Robot;
+import org.usfirst.frc.team1732.robot.commands.drivetrain.BrakeDriveNoShift;
 import org.usfirst.frc.team1732.robot.commands.drivetrain.encoder.DriveEncodersGetSetpointAtRuntime;
-import org.usfirst.frc.team1732.robot.commands.drivetrain.encoder.DriveEncodersWithBraking;
 import org.usfirst.frc.team1732.robot.commands.drivetrain.encoder.ResetEncoderPID;
 import org.usfirst.frc.team1732.robot.commands.drivetrain.encoder.SetEncoderPID;
 import org.usfirst.frc.team1732.robot.commands.gearIntake.base.motor.GearIntakeOutTime;
@@ -16,47 +16,48 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class EncoderPlaceGear extends CommandGroup {
 
-	public EncoderPlaceGear(double driveForwardDistance, double driveBackDistance) {
-		this(driveForwardDistance, driveBackDistance, driveBackDistance);
-	}
+    public EncoderPlaceGear(double driveForwardDistance, double driveBackDistance) {
+	this(driveForwardDistance, driveBackDistance, driveBackDistance);
+    }
 
-	public EncoderPlaceGear(double driveForwardDistance, double leftDriveBackDistance, double rightDriveBackDistance) {
-		this(() -> driveForwardDistance, () -> leftDriveBackDistance, () -> rightDriveBackDistance);
-	}
+    public EncoderPlaceGear(double driveForwardDistance, double leftDriveBackDistance, double rightDriveBackDistance) {
+	this(() -> driveForwardDistance, () -> leftDriveBackDistance, () -> rightDriveBackDistance);
+    }
 
-	public EncoderPlaceGear(DoubleSupplier driveForwardDistance, DoubleSupplier driveBackDistance) {
-		this(driveForwardDistance, driveBackDistance, driveBackDistance);
-	}
+    public EncoderPlaceGear(DoubleSupplier driveForwardDistance, DoubleSupplier driveBackDistance) {
+	this(driveForwardDistance, driveBackDistance, driveBackDistance);
+    }
 
-	public EncoderPlaceGear(DoubleSupplier driveForwardDistance, DoubleSupplier leftDriveBackDistance,
-			DoubleSupplier rightDriveBackDistance) {
-		// drive into gear peg
-		// addSequential(new TurnWithVision(0));
-		addSequential(new SetEncoderPID(0.1, 0, 0));
-		addSequential(new DriveEncodersWithBraking(driveForwardDistance));
-		addSequential(new Wait(0.1));
+    public EncoderPlaceGear(DoubleSupplier driveForwardDistance, DoubleSupplier leftDriveBackDistance,
+	    DoubleSupplier rightDriveBackDistance) {
+	// drive into gear peg
+	// addSequential(new TurnWithVision(0));
+	addSequential(new SetEncoderPID(0.1, 0, 0));
+	addSequential(new DriveEncodersGetSetpointAtRuntime(driveForwardDistance));
+	addSequential(new BrakeDriveNoShift());
+	addSequential(new Wait(0.1));
 
-		// place gear, drive back at same time
+	// place gear, drive back at same time
 
-		addSequential(new GearIntakeSetDown());
-		addParallel(new CommandGroup() {
-			{
-				addSequential(new GearIntakeOutTime(.5));
-				addSequential(new GearIntakeSetUp());
-			}
-		});
-		addSequential(new SetEncoderPID(.05, 0, 0));
-		addSequential(new DriveEncodersGetSetpointAtRuntime(leftDriveBackDistance, rightDriveBackDistance));
-		addSequential(new ResetEncoderPID());
-	}
+	addSequential(new GearIntakeSetDown());
+	addParallel(new CommandGroup() {
+	    {
+		addSequential(new GearIntakeOutTime(.5));
+		addSequential(new GearIntakeSetUp());
+	    }
+	});
+	addSequential(new SetEncoderPID(.05, 0, 0));
+	addSequential(new DriveEncodersGetSetpointAtRuntime(leftDriveBackDistance, rightDriveBackDistance));
+	addSequential(new ResetEncoderPID());
+    }
 
-	@Override
-	public void interrupted() {
-		end();
-	}
+    @Override
+    public void interrupted() {
+	end();
+    }
 
-	@Override
-	public void end() {
-		Robot.driveTrain.resetEncoderPID();
-	}
+    @Override
+    public void end() {
+	Robot.driveTrain.resetEncoderPID();
+    }
 }
