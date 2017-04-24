890c20d0d4d2007416603e22d0df43984d2cd72d
JayShower <lacrossejason@gmail.com>
2017-04-24 07:57:06 -0500

    Low gear brake drive no longer applies motor voltage

--- 2017 Practice Bot/src/org/usfirst/frc/team1732/robot/commands/drivetrain/BrakeDrive.java
+++ 2017 Practice Bot/src/org/usfirst/frc/team1732/robot/commands/drivetrain/BrakeDrive.java
@@ -9,38 +9,45 @@ import edu.wpi.first.wpilibj.command.Command;
  */
 public class BrakeDrive extends Command {
 
-	public BrakeDrive() {
-		// Use requires() here to declare subsystem dependencies
-		// eg. requires(chassis);
-		requires(Robot.driveTrain);
-	}
+    public BrakeDrive() {
+	// Use requires() here to declare subsystem dependencies
+	// eg. requires(chassis);
+	requires(Robot.driveTrain);
+    }
 
-	// Called just before this Command runs the first time
-	@Override
-	protected void initialize() {
-		Robot.driveTrain.shiftLowGear();
-	}
+    // Called just before this Command runs the first time
+    @Override
+    protected void initialize() {
+	System.out.println("start braking");
+	Robot.driveTrain.shiftLowGear();
+	Robot.driveTrain.driveRaw(0, 0);
+    }
 
-	public static final double BRAKE_P = (-1 / (13.5 * 12)) * 4;
+    public static final double BRAKE_P = (1 / (13.5 * 12)) * 4;
 
-	// Called repeatedly when this Command is scheduled to run
-	@Override
-	protected void execute() {
-		double leftV = Robot.driveTrain.getLeftVelocity();
-		double rightV = Robot.driveTrain.getRightVelocity();
-		Robot.driveTrain.driveRaw(leftV * BRAKE_P, rightV * BRAKE_P);
-	}
+    // Called repeatedly when this Command is scheduled to run
+    @Override
+    protected void execute() {
+	// double leftV = Robot.driveTrain.getLeftVelocity();
+	// double rightV = Robot.driveTrain.getRightVelocity();
+	// double averageV = (leftV + rightV) / 2.0;
+	// double output = averageV * BRAKE_P;
+	// double left = -Math.copySign(output, leftV);
+	// double right = -Math.copySign(output, rightV);
+	// Robot.driveTrain.driveRaw(left, right);
+    }
 
-	// Make this return true when this Command no longer needs to run execute()
-	@Override
-	protected boolean isFinished() {
-		return Math.abs(Robot.driveTrain.getLeftVelocity()) < 2 && Math.abs(Robot.driveTrain.getRightVelocity()) < 2;
-	}
+    // Make this return true when this Command no longer needs to run execute()
+    @Override
+    protected boolean isFinished() {
+	return Math.abs(Robot.driveTrain.getLeftVelocity()) < 1 && Math.abs(Robot.driveTrain.getRightVelocity()) < 1;
+    }
 
-	// Called once after isFinished returns true
-	@Override
-	protected void end() {
-		Robot.driveTrain.shiftHighGear();
-	}
+    // Called once after isFinished returns true
+    @Override
+    protected void end() {
+	System.out.println("end braking");
+	Robot.driveTrain.shiftHighGear();
+    }
 
 }
