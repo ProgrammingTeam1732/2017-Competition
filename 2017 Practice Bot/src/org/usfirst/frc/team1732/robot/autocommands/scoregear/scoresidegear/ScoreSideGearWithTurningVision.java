4476ea3f63f8fd33cf6144635881833d140e3def
JayShower <lacrossejason@gmail.com>
2017-04-24 08:13:53 -0500

    Simplified to use base commands

--- 2017 Practice Bot/src/org/usfirst/frc/team1732/robot/autocommands/scoregear/scoresidegear/ScoreSideGearWithTurningVision.java
+++ 2017 Practice Bot/src/org/usfirst/frc/team1732/robot/autocommands/scoregear/scoresidegear/ScoreSideGearWithTurningVision.java
@@ -1,7 +1,7 @@
 package org.usfirst.frc.team1732.robot.autocommands.scoregear.scoresidegear;
 
+import org.usfirst.frc.team1732.robot.autocommands.scoregear.scoresidegear.base.ScoreSideGearPart1;
 import org.usfirst.frc.team1732.robot.commands.gearIntake.commandgroups.InitGearIntake;
-import org.usfirst.frc.team1732.robot.commands.helpercommands.Wait;
 import org.usfirst.frc.team1732.robot.commands.placegear.EncoderPlaceGear;
 import org.usfirst.frc.team1732.robot.commands.vision.lights.TurnLightsOn;
 import org.usfirst.frc.team1732.robot.commands.vision.movement.DitherTurnWithVision;
@@ -13,17 +13,13 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
  */
 public class ScoreSideGearWithTurningVision extends CommandGroup {
 
-	public ScoreSideGearWithTurningVision(boolean isLeft) {
-		addSequential(new InitGearIntake());
-		if (isLeft) {
-			addSequential(new ScoreSideGearPart1Left());
-		} else {
-			addSequential(new ScoreSideGearPart1Right());
-		}
-		addSequential(new TurnLightsOn());
-		addSequential(new Wait(.5));
-		addSequential(new DitherTurnWithVision(0));
-		addSequential(new EncoderPlaceGear(15, -30));
-	}
+    public ScoreSideGearWithTurningVision(boolean isLeft) {
+	addSequential(new InitGearIntake());
+	addSequential(new ScoreSideGearPart1(isLeft));
+	addSequential(new TurnLightsOn());
+	// addSequential(new Wait(.5));
+	addSequential(new DitherTurnWithVision(0));
+	addSequential(new EncoderPlaceGear(15, -30));
+    }
 
 }
