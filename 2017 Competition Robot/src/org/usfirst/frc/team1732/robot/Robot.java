a4d4d3e0b0d6c1bca98c8a9fab84de7ec9142d13
JayShower <lacrossejason@gmail.com>
2017-04-25 20:22:33 -0500

    Robot.java file

--- 2017 Competition Robot/src/org/usfirst/frc/team1732/robot/Robot.java
+++ 2017 Competition Robot/src/org/usfirst/frc/team1732/robot/Robot.java
@@ -1,7 +1,12 @@
 
 package org.usfirst.frc.team1732.robot;
 
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
 import org.usfirst.frc.team1732.robot.autocommands.AutoChooser;
+import org.usfirst.frc.team1732.robot.commands.SetRobotToStartState;
 import org.usfirst.frc.team1732.robot.commands.ballsystem.ballintake.motor.BallIntakeSetIn;
 import org.usfirst.frc.team1732.robot.commands.ballsystem.ballintake.motor.BallIntakeSetOut;
 import org.usfirst.frc.team1732.robot.commands.ballsystem.ballintake.motor.BallIntakeSetStop;
@@ -15,6 +20,7 @@ import org.usfirst.frc.team1732.robot.commands.ballsystem.flywheel.EnableFlywhee
 import org.usfirst.frc.team1732.robot.commands.ballsystem.flywheel.manual.FlywheelForward;
 import org.usfirst.frc.team1732.robot.commands.ballsystem.flywheel.manual.FlywheelReverse;
 import org.usfirst.frc.team1732.robot.commands.ballsystem.flywheel.manual.FlywheelStop;
+import org.usfirst.frc.team1732.robot.commands.ballsystem.shooting.ShuffleBallsWithWait;
 import org.usfirst.frc.team1732.robot.commands.climber.ArmSetIn;
 import org.usfirst.frc.team1732.robot.commands.climber.ArmSetOut;
 import org.usfirst.frc.team1732.robot.commands.climber.ClimberSetStop;
@@ -57,9 +63,14 @@ import org.usfirst.frc.team1732.robot.subsystems.Feeder;
 import org.usfirst.frc.team1732.robot.subsystems.Flywheel;
 import org.usfirst.frc.team1732.robot.subsystems.GearIntake;
 import org.usfirst.frc.team1732.robot.subsystems.PixyCamera;
+import org.usfirst.frc.team1732.robot.subsystems.Wings;
 import org.usfirst.frc.team1732.robot.triggers.Triggers;
 import org.usfirst.frc.team1732.robot.vision.VisionMain;
 
+import edu.wpi.cscore.CvSink;
+import edu.wpi.cscore.CvSource;
+import edu.wpi.cscore.UsbCamera;
+import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.IterativeRobot;
@@ -76,248 +87,322 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  */
 public class Robot extends IterativeRobot {
 
-	public static OI			oi;
-	public static DriveTrain	driveTrain;
-	public static BallIntake	ballIntake;
-	public static Climber		climber;
-	public static Feeder		feeder;
-	public static Flywheel		flywheel;
-	public static GearIntake	gearIntake;
-	public static Arm			arm;
-	public static PixyCamera	pixyCamera;
-	public static Triggers		triggers;
+    public static OI oi;
+    public static DriveTrain driveTrain;
+    public static BallIntake ballIntake;
+    public static Climber climber;
+    public static Feeder feeder;
+    public static Flywheel flywheel;
+    public static GearIntake gearIntake;
+    public static Arm arm;
+    public static Wings wings;
+    public static PixyCamera pixyCamera;
+    public static Triggers triggers;
 
-	public static VisionMain		visionMain;
-	private static MySmartDashboard	dashboard;
+    public static VisionMain visionMain;
+    private static MySmartDashboard dashboard;
 
-	private static AutoChooser					autoChooser;
-	private static SmartDashboardItem<Boolean>	isRedAlliance;
-	private static SmartDashboardItem<Double>	distanceToDriveBackForTwoGear;
+    private static AutoChooser autoChooser;
+    public static SmartDashboardItem<Boolean> isRedAlliance;
+    public static SmartDashboardItem<Double> autoWaitTime;
+    public static SmartDashboardItem<Double> startOnWallAndShootDistance;
 
-	/**
-	 * This function is run when the robot is first started up and should be
-	 * used for any initialization code. - written by WPIlib people
-	 */
-	@Override
-	public void robotInit() {
-		try {
-			// initialize subsystems - always do this first
-			driveTrain = new DriveTrain();
-			flywheel = new Flywheel();
-			ballIntake = new BallIntake();
-			climber = new Climber();
-			feeder = new Feeder();
-			arm = new Arm();
-			gearIntake = new GearIntake();
-			pixyCamera = new PixyCamera();
-			triggers = new Triggers();
-			autoChooser = new AutoChooser();
+    /**
+     * This function is run when the robot is first started up and should be
+     * used for any initialization code. - written by WPIlib people
+     */
+    @Override
+    public void robotInit() {
+	try {
+	    // initialize smartdashboard
+	    initializeMySmartDashboardItems();
+	    initializeSubsystems();
+	    initializeVision();
+	    initializeInput();
 
-			oi = new OI();
-			visionMain = new VisionMain();
+	    // Add items to smartdashboard
+	    addSubsystemsToSmartDashboard();
+	    addAutonomousToSmartDashboard();
+	    addTestingToSmartDashbaord();
+	    addCamera();
+	    dashboard.addItem(SmartDashboardItem.newNumberSender("robotPeriodic() frequency ms", this::getFrequency));
 
-			// Smartdashboard code
-			dashboard = new MySmartDashboard();
-			// Add items to smartdashboard
-			addSubsystemsToSmartDashboard();
-			addAutonomousToSmartDashboard();
-			addTestingToSmartDashbaord();
-			// dashboard.addItem(SmartDashboardItem.newDoubleReciever("Light
-			// Voltage", 0.0, pixyCamera::setLightVoltage));
-			dashboard.addItem(SmartDashboardItem.newNumberSender("robotPeriodic() frequency ms", this::getFrequency));
-			// dashboard.addItem(SmartDashboardItem.newDoubleReciever("Feeder
-			// Speed", 1.0, feeder::setSpeed));
-			// dashboard.addItem(SmartDashboardItem.newDoubleReciever("Intake
-			// Speed", -1.0, ballIntake::setSpeed));
-			SmartDashboard.putData(new TestVisionMain());
-			// addCamera();
+	    // initially sends items that have been added to driverstation
+	    dashboard.init();
+	} catch (Exception e) {
+	    e.printStackTrace();
+	}
+    }
 
-			// Initialize smartdashboard
-			dashboard.init();
-		} catch (Exception e) {
-			e.printStackTrace();
+    @Override
+    public void robotPeriodic() {
+	dashboard.run();
+    }
+
+    @Override
+    public void disabledInit() {
+	setRobotToDefaultStates();
+    }
+
+    @Override
+    public void disabledPeriodic() {
+	Scheduler.getInstance().run();
+    }
+
+    @Override
+    public void autonomousInit() {
+	Scheduler.getInstance().removeAll();
+	autoChooser.getSelected().start();
+    }
+
+    @Override
+    public void autonomousPeriodic() {
+	Scheduler.getInstance().run();
+    }
+
+    @Override
+    public void teleopInit() {
+	Scheduler.getInstance().removeAll(); // Cancels commands
+	// setRobotToDefaultStates();
+	new SetRobotToStartState().start();
+    }
+
+    @Override
+    public void teleopPeriodic() {
+	Scheduler.getInstance().run();
+    }
+
+    @Override
+    public void testInit() {
+    }
+
+    @Override
+    public void testPeriodic() {
+	LiveWindow.run();
+    }
+
+    private void initializeMySmartDashboardItems() {
+	dashboard = new MySmartDashboard();
+
+	autoWaitTime = dashboard.addItem(SmartDashboardItem.newDoubleReciever("Auto wait time", 0.0));
+	startOnWallAndShootDistance = dashboard
+		.addItem(SmartDashboardItem.newDoubleReciever("Start wall and shoot wait distance", 100.0));
+	isRedAlliance = dashboard.addItem(SmartDashboardItem.newBooleanSender("Is Red Alliance?",
+		() -> DriverStation.getInstance().getAlliance().equals(Alliance.Red)));
+    }
+
+    private void initializeSubsystems() {
+	driveTrain = new DriveTrain();
+	flywheel = new Flywheel();
+	ballIntake = new BallIntake();
+	climber = new Climber();
+	feeder = new Feeder();
+	arm = new Arm();
+	gearIntake = new GearIntake();
+	wings = new Wings();
+    }
+
+    private void initializeVision() {
+	pixyCamera = new PixyCamera();
+	visionMain = new VisionMain();
+    }
+
+    private void initializeInput() {
+	triggers = new Triggers();
+	oi = new OI();
+    }
+
+    private void addSubsystemsToSmartDashboard() {
+	driveTrain.addToSmartDashboard(dashboard);
+	flywheel.addToSmartDashboard(dashboard);
+	ballIntake.addToSmartDashboard(dashboard);
+	climber.addToSmartDashboard(dashboard);
+	feeder.addToSmartDashboard(dashboard);
+	arm.addToSmartDashboard(dashboard);
+	wings.addToSmartDashboard(dashboard);
+	gearIntake.addToSmartDashboard(dashboard);
+	visionMain.addToSmartDashboard(dashboard);
+    }
+
+    private void addAutonomousToSmartDashboard() {
+	autoChooser = new AutoChooser();
+	autoChooser.addToSmartDashboard(dashboard);
+	dashboard.addItem(SmartDashboardItem.newStringSender("AUTO:", () -> {
+	    try {
+		return autoChooser.getSelected().name();
+	    } catch (Exception e) {
+		return "Error";
+	    }
+	}));
+    }
+
+    private void addTestingToSmartDashbaord() {
+	SmartDashboard.putData(new ShuffleBallsWithWait());
+	SmartDashboard.putData(new TestVisionMain());
+
+	SmartDashboard.putData(new FlywheelForward());
+	SmartDashboard.putData(new FlywheelReverse());
+	SmartDashboard.putData(new FlywheelStop());
+
+	SmartDashboard.putData(new EnableFlywheel());
+	SmartDashboard.putData(new DisableFlywheel());
+
+	SmartDashboard.putData(new MotorLTBackForward());
+	SmartDashboard.putData(new MotorLTBackReverse());
+	SmartDashboard.putData(new MotorLTBackStop());
+
+	SmartDashboard.putData(new MotorLTBottomForward());
+	SmartDashboard.putData(new MotorLTBottomReverse());
+	SmartDashboard.putData(new MotorLTBottomStop());
+
+	SmartDashboard.putData(new MotorLTFrontForward());
+	SmartDashboard.putData(new MotorLTFrontReverse());
+	SmartDashboard.putData(new MotorLTFrontStop());
+
+	SmartDashboard.putData(new MotorRTBackForward());
+	SmartDashboard.putData(new MotorRTBackReverse());
+	SmartDashboard.putData(new MotorRTBackStop());
+
+	SmartDashboard.putData(new MotorRTBottomForward());
+	SmartDashboard.putData(new MotorRTBottomReverse());
+	SmartDashboard.putData(new MotorRTBottomStop());
+
+	SmartDashboard.putData(new MotorRTFrontForward());
+	SmartDashboard.putData(new MotorRTFrontReverse());
+	SmartDashboard.putData(new MotorRTFrontStop());
+
+	SmartDashboard.putData(new ArmSetOut());
+	SmartDashboard.putData(new ArmSetIn());
+
+	SmartDashboard.putData(new BallIntakeSetDown());
+	SmartDashboard.putData(new BallIntakeSetUp());
+
+	SmartDashboard.putData(new ShiftHigh());
+	SmartDashboard.putData(new ShiftLow());
+
+	SmartDashboard.putData(new GearIntakeSetUp());
+	SmartDashboard.putData(new GearIntakeSetDown());
+
+	SmartDashboard.putData(new GearIntakeSetStopperOut());
+	SmartDashboard.putData(new GearIntakeSetStopperIn());
+
+	SmartDashboard.putData(new FeederSetIn());
+	SmartDashboard.putData(new FeederSetOut());
+	SmartDashboard.putData(new FeederSetStop());
+
+	SmartDashboard.putData(new ClimberSetStop());
+	SmartDashboard.putData(new ClimberSetUp());
+
+	SmartDashboard.putData(new BallIntakeSetOut());
+	SmartDashboard.putData(new BallIntakeSetIn());
+	SmartDashboard.putData(new BallIntakeSetStop());
+
+	SmartDashboard.putData(new GearIntakeSetOut());
+	SmartDashboard.putData(new GearIntakeSetIn());
+	SmartDashboard.putData(new GearIntakeSetStop());
+    }
+
+    private long startTime = System.currentTimeMillis();
+
+    private long getFrequency() {
+	long start = startTime;
+	startTime = System.currentTimeMillis();
+	return startTime - start;
+    }
+
+    private void addCamera() {
+	int width = 320;
+	int height = 240;
+	int thickness = 10;
+	Scalar color = new Scalar(0, 255, 0);
+	Runnable visionRun = new Runnable() {
+	    @Override
+	    public void run() {
+		// Get the UsbCamera from CameraServer
+		UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
+		// Set the resolution
+		camera.setResolution(width, height);
+
+		// Get a CvSink. This will capture Mats from the camera
+		CvSink cvSink = CameraServer.getInstance().getVideo(camera);
+		// Setup a CvSource. This will send images back to the Dashboard
+		CvSource outputStream = CameraServer.getInstance().putVideo("Video", width, height);
+
+		// Mats are very memory expensive. Lets reuse this Mat.
+		// Mats are very memory expensive. Lets reuse this Mat.
+		Mat mat = new Mat();
+
+		// This cannot be 'true'. The program will never exit if it is.
+		// This
+		// lets the robot stop this thread when restarting robot code or
+		// deploying.
+		while (!Thread.interrupted()) {
+		    // Tell the CvSink to grab a frame from the camera and put
+		    // it
+		    // in the source mat. If there is an error notify the
+		    // output.
+		    if (cvSink.grabFrame(mat) == 0) {
+			// Send the output the error.
+			outputStream.notifyError(cvSink.getError());
+			System.out.println("Error getting frame");
+			// skip the rest of the current iteration
+			continue;
+		    }
+		    // Put a border on the image
+		    if (gearIntake.gearIsIn()) {// gearIntake.gearIsHeld() ||) {
+			// upper edge
+			Imgproc.rectangle(mat, new Point(0, 0), new Point(width, thickness), color, thickness);
+			// left edge
+			Imgproc.rectangle(mat, new Point(0, 0), new Point(thickness, height), color, thickness);
+			// right edge
+			Imgproc.rectangle(mat, new Point(width - thickness, 0), new Point(width, height), color,
+				thickness);
+			// bottom edge
+			Imgproc.rectangle(mat, new Point(0, height - thickness), new Point(width, height), color,
+				thickness);
+		    }
+		    // add lines for karl target
+		    // Scalar lineColor = new Scalar(0, 0, 0);
+		    // Imgproc.line( mat, new Point((int) (width * 0.34651), 0),
+		    // new Point((int) (width * 0.031217), height), lineColor,
+		    // 2);
+		    // Imgproc.line( mat, new Point((int) (width * 0.537981),
+		    // 0),
+		    // new Point((int) (width * 0.816857), height), lineColor,
+		    // 2);
+		    // Give the output stream a new image to display
+		    outputStream.putFrame(mat);
 		}
-	}
+	    }
 
-	@Override
-	public void robotPeriodic() {
-		dashboard.run();
-	}
+	};
+	Thread visionThread = new Thread(visionRun);
+	visionThread.setDaemon(true);
+	visionThread.start();
 
-	@Override
-	public void disabledInit() {}
+    }
 
-	@Override
-	public void disabledPeriodic() {
-		Scheduler.getInstance().run();
-	}
-
-	@Override
-	public void autonomousInit() {
-		Scheduler.getInstance().removeAll();
-		// new TurnLightsOn().start();
-		autoChooser.getSelected().start();
-	}
-
-	@Override
-	public void autonomousPeriodic() {
-		Scheduler.getInstance().run();
-	}
-
-	@Override
-	public void teleopInit() {
-		Scheduler.getInstance().removeAll(); // Cancels commands
-		Robot.driveTrain.clearEncoderIntgral();
-		Robot.driveTrain.clearGyroIntgral();
-
-	}
-
-	@Override
-	public void teleopPeriodic() {
-		Scheduler.getInstance().run();
-	}
-
-	@Override
-	public void testInit() {}
-
-	@Override
-	public void testPeriodic() {
-		LiveWindow.run();
-	}
-
-	private void addSubsystemsToSmartDashboard() {
-		driveTrain.addToSmartDashboard(dashboard);
-		flywheel.addToSmartDashboard(dashboard);
-		ballIntake.addToSmartDashboard(dashboard);
-		climber.addToSmartDashboard(dashboard);
-		feeder.addToSmartDashboard(dashboard);
-		arm.addToSmartDashboard(dashboard);
-		gearIntake.addToSmartDashboard(dashboard);
-		visionMain.addToSmartDashboard(dashboard);
-		autoChooser.addToSmartDashboard(dashboard);
-	}
-
-	public static boolean isRedAlliance() {
-		return isRedAlliance.getValue();
-	}
-
-	public static double getDistanceToDriveBackForTwoGear() {
-		return distanceToDriveBackForTwoGear.getValue();
-	}
-
-	private void addAutonomousToSmartDashboard() {
-		isRedAlliance = dashboard.addItem(SmartDashboardItem
-				.newBooleanSender(	"Is Red Alliance?",
-									() -> DriverStation.getInstance().getAlliance().equals(Alliance.Red)));
-
-		distanceToDriveBackForTwoGear = dashboard
-				.addItem(SmartDashboardItem.newDoubleReciever("Two Gear Auto: inches from wall to go to", 50.0));
-
-		dashboard.addItem(SmartDashboardItem.newStringSender("Selected Auto Command", () -> {
-			try {
-				return autoChooser.getSelected().getName();
-			} catch (Exception e) {
-				return "null";
-			}
-		}));
-	}
-
-	private void addTestingToSmartDashbaord() {
-		//		SmartDashboard.putData(new ShuffleBallsWithWait());
-		SmartDashboard.putData(new FlywheelForward());
-		SmartDashboard.putData(new FlywheelReverse());
-		SmartDashboard.putData(new FlywheelStop());
-
-		SmartDashboard.putData(new EnableFlywheel());
-		SmartDashboard.putData(new DisableFlywheel());
-
-		SmartDashboard.putData(new MotorLTBackForward());
-		SmartDashboard.putData(new MotorLTBackReverse());
-		SmartDashboard.putData(new MotorLTBackStop());
-
-		SmartDashboard.putData(new MotorLTBottomForward());
-		SmartDashboard.putData(new MotorLTBottomReverse());
-		SmartDashboard.putData(new MotorLTBottomStop());
-
-		SmartDashboard.putData(new MotorLTFrontForward());
-		SmartDashboard.putData(new MotorLTFrontReverse());
-		SmartDashboard.putData(new MotorLTFrontStop());
-
-		SmartDashboard.putData(new MotorRTBackForward());
-		SmartDashboard.putData(new MotorRTBackReverse());
-		SmartDashboard.putData(new MotorRTBackStop());
-
-		SmartDashboard.putData(new MotorRTBottomForward());
-		SmartDashboard.putData(new MotorRTBottomReverse());
-		SmartDashboard.putData(new MotorRTBottomStop());
-
-		SmartDashboard.putData(new MotorRTFrontForward());
-		SmartDashboard.putData(new MotorRTFrontReverse());
-		SmartDashboard.putData(new MotorRTFrontStop());
-
-		SmartDashboard.putData(new ArmSetOut());
-		SmartDashboard.putData(new ArmSetIn());
-
-		SmartDashboard.putData(new BallIntakeSetDown());
-		SmartDashboard.putData(new BallIntakeSetUp());
-
-		SmartDashboard.putData(new ShiftHigh());
-		SmartDashboard.putData(new ShiftLow());
-
-		SmartDashboard.putData(new GearIntakeSetUp());
-		SmartDashboard.putData(new GearIntakeSetDown());
-
-		SmartDashboard.putData(new GearIntakeSetStopperOut());
-		SmartDashboard.putData(new GearIntakeSetStopperIn());
-
-		SmartDashboard.putData(new FeederSetIn());
-		SmartDashboard.putData(new FeederSetOut());
-		SmartDashboard.putData(new FeederSetStop());
-
-		SmartDashboard.putData(new ClimberSetStop());
-		SmartDashboard.putData(new ClimberSetUp());
-
-		SmartDashboard.putData(new BallIntakeSetOut());
-		SmartDashboard.putData(new BallIntakeSetIn());
-		SmartDashboard.putData(new BallIntakeSetStop());
-
-		SmartDashboard.putData(new GearIntakeSetOut());
-		SmartDashboard.putData(new GearIntakeSetIn());
-		SmartDashboard.putData(new GearIntakeSetStop());
-	}
-
-	private long startTime = System.currentTimeMillis();
-
-	private long getFrequency() {
-		long start = startTime;
-		startTime = System.currentTimeMillis();
-		return startTime - start;
-	}
-
-	@SuppressWarnings("unused")
-	private void addCamera() {
-		// Thread visionThread = new Thread(() -> {
-		//
-		// // Setup a CvSource. This will send images back to the Dashboard
-		// CvSource outputStream = CameraServer.getInstance().putVideo("Video",
-		// 320, 240);
-		//
-		// // Mats are very memory expensive. Lets reuse this Mat.
-		// Mat mat = new Mat();
-		//
-		// // This cannot be 'true'. The program will never exit if it is. This
-		// // lets the robot stop this thread when restarting robot code or
-		// // deploying.
-		// while (!Thread.interrupted()) {
-		// // Tell the CvSink to grab a frame from the camera and put it
-		// // in the source mat. If there is an error notify the output.
-		//
-		// mat = new Mat(240, 320, org.opencv.core.CvType.CV_8UC(3));
-		// Imgproc.rectangle(img, pt1, pt2, color);
-		// outputStream.putFrame(mat);
-		// }
-		// });
-		// visionThread.setDaemon(true);
-		// visionThread.start();
-	}
+    /**
+     * Sets the robot to its default states Default states <br>
+     * -solenoid default positions (what position they are when in disabled
+     * mode) <br>
+     * -motors turned off
+     */
+    private void setRobotToDefaultStates() {
+	driveTrain.clearEncoderIntgral();
+	driveTrain.clearGyroIntgral();
+	// moters
+	feeder.setStop(); // stop feeder
+	flywheel.disableAutoControl(); // turn off flywheel
+	driveTrain.driveRaw(0, 0);
+	climber.setStop();
+	gearIntake.setStop();
+	ballIntake.setSpeedStop();
+	// pnuematics
+	arm.setIn();
+	gearIntake.setUp();
+	ballIntake.setPositionDown();
+	driveTrain.shiftHighGear();
+	wings.setIn();
+    }
 }
